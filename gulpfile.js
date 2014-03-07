b26bb0d170d627e5c341936d3a1caccf1f43a04b
oyvodega <oyvind.odegard@finn.no>
2014-03-07 20:06:48 +0100

    - Move code
    - Add gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+var gulp       = require('gulp');
+var gulpCoffee = require('gulp-coffee');
+var gulpConcat = require('gulp-concat');
+var gulpUglify = require('gulp-uglify');
+
+var paths = {
+  scripts: ['src/main/webapp/assets/scripts/**.coffee']
+};
+
+gulp.task('scripts', function() {
+   return gulp.src(paths.scripts)
+       .pipe(gulpCoffee())
+       .pipe(gulpUglify())
+       .pipe(gulpConcat('out.min.js'))
+       .pipe(gulp.dest(''));
+});
+
+gulp.task('default', ['scripts']);
