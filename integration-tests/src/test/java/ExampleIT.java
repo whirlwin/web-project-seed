e84dde61c61007f1f1b4bf4e6eb5de629a318c22
whirlwin <whirlwin90@gmail.com>
2014-04-07 20:55:36 +0200

    Use generic test runner.

--- integration-tests/src/test/java/ExampleIT.java
+++ integration-tests/src/test/java/ExampleIT.java
@@ -3,14 +3,14 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.runners.MockitoJUnitRunner;
+import org.junit.runners.JUnit4;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
-@RunWith(MockitoJUnitRunner.class)
+@RunWith(JUnit4.class)
 public final class ExampleIT extends TestCase {
 
     private static final String BASE_URL = "http://localhost:10001/web-project-seed-integration-tests";
